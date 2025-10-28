b4c192e3ae353487381b884ba7c2baa2cc7da96c
RajanAcy <139981341+RajanAcy@users.noreply.github.com>
2025-10-28 13:59:23 +0630

    Swift9.6

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteSingleFile()
+  ],
+  base: '/git_swift_pos_v3/'
+})
